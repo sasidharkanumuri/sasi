dfbd732970dbcc0cf3a68ae00d536340f024a6cb
“YOUR <kanumuri1994@gmail.com>
2016-08-03 19:54:41 -0400

    loggingsample
    add

---
+++ LoggingSample/src/main/java/com/sasi/training/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.sasi.training;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by user on 8/1/2016.
+ */
+public class HelloWorld {
+    private static final Logger log= LoggerFactory.getLogger(HelloWorld.class);
+public static void main(String []args){
+log.info("hello world");
+
+
+        }
+}
